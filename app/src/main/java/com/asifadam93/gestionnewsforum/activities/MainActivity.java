9842bd99cd5814823a16ac303f2a4680a18a5aa5
fisamada39 <asifadam93@hotmail.fr>
2017-07-12 22:37:31 +0200

    change fragments via bottom nav bar

--- app/src/main/java/com/asifadam93/gestionnewsforum/activities/MainActivity.java
+++ app/src/main/java/com/asifadam93/gestionnewsforum/activities/MainActivity.java
@@ -3,15 +3,29 @@ package com.asifadam93.gestionnewsforum.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 
 import com.asifadam93.gestionnewsforum.R;
+import com.asifadam93.gestionnewsforum.fragments.UserFragment;
+import com.asifadam93.gestionnewsforum.fragments.NewsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    private static final String FRAGMENT_USER = "user";
+    private static final String FRAGMENT_NEWS = "news";
+    private static final String FRAGMENT_TOPIC = "topic";
+
     private BottomNavigationView bottomNavigationView;
+    private List<UserFragment> fragments = new ArrayList<>(3);
+
+    private NewsFragment newsFragment;
+    private UserFragment userFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,27 +34,60 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        //set news as default position
+        showNewsFragment();
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
 
             case R.id.bottom_bar_user:
-                Log.i("bottonNav","bottom_bar_user");
+                Log.i("bottonNav", "bottom_bar_user");
+                showUserFragment();
                 break;
 
             case R.id.bottom_bar_news:
-                Log.i("bottonNav","bottom_bar_news");
+                Log.i("bottonNav", "bottom_bar_news");
+                showNewsFragment();
                 break;
 
             case R.id.bottom_bar_topic:
-                Log.i("bottonNav","bottom_bar_topic");
+                Log.i("bottonNav", "bottom_bar_topic");
+                // TODO: 12/07/2017
                 break;
 
         }
 
         return false;
     }
+
+    private void showNewsFragment() {
+
+        if (newsFragment == null) {
+            newsFragment = new NewsFragment();
+        }
+
+        replaceFragment(newsFragment, FRAGMENT_NEWS);
+    }
+
+    private void showUserFragment() {
+
+        if (userFragment == null) {
+            userFragment = new UserFragment();
+        }
+
+        replaceFragment(userFragment, FRAGMENT_USER);
+    }
+
+
+    private void replaceFragment(Fragment fragment, String tag) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_holder, fragment, tag)
+                .commit();
+    }
+
 }
---
+++ app/src/main/java/com/asifadam93/gestionnewsforum/fragments/UserFragment.java
@@ -0,0 +1,35 @@
+package com.asifadam93.gestionnewsforum.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.asifadam93.gestionnewsforum.R;
+
+
+/**
+ * Created by Asifadam93 on 12/07/2017.
+ */
+
+public class UserFragment extends Fragment {
+
+    private TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_bottom_nav, container, false);
+
+        textView = (TextView) rootView.findViewById(R.id.textView);
+        textView.setText("User");
+
+        /*String title = getArguments().getString(ARG_TITLE, "");
+        textView.setText(title);*/
+        return rootView;
+    }
+}
